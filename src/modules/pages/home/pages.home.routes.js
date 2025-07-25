019e134cdf67f29b0fdb7fdb2ca5319c03f1401b
Piu@96 <Sutrishna@96>
2025-07-25 12:06:12 +0530

    update

--- src/modules/pages/home/pages.home.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const PagesController = require('./pages.home.controller');
-const menuController = new PagesController();
-const { validatePageRequest } = require('./pages.home.validator');
-router.get("/list",validatePageRequest, menuController.pagesCms);
-
-module.exports = router;
